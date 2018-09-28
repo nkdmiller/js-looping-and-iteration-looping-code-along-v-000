44bc2705ec05cf16c9c6aa561ee888fcbef8ca42
Nate Miller <natekmiller96@gmail.com>
2018-09-28 01:40:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solutions in this file
+
+function printBadges(array){
+  for (let count = 0; count < array.length; count++){console.log("Welcome " + array[count] + "! You are employee #" + (count+1) + ".");}
+}
