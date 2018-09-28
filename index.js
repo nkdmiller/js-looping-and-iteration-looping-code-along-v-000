740f9a7c7184599f8be13840193b75ebd5715b44
Nate Miller <natekmiller96@gmail.com>
2018-09-28 01:53:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function printBadges(array){
+  for (let count = 0; count < array.length; count++)
+  {
+    console.log("Welcome " + array[count] + "! You are employee #" + (count+1) + ".");
+    
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5){
+    tails++;
+    
+  } 
+  return( "You got " + tails + " tails in a row!")
+}
+
