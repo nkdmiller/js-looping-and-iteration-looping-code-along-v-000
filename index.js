2030fbdfa51537af620271530330f14d3a2de46a
Nate Miller <natekmiller96@gmail.com>
2018-09-28 01:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
 function printBadges(array){
-  for (let count = 0; count < array.length; count++){console.log("Welcome " + array[count] + "! You are employee #" + (count+1) + ".");}
+  for (let count = 0; count < array.length; count++)
+  {
+    console.log("Welcome " + array[count] + "! You are employee #" + (count+1) + ".");
+    
+  }
 }
